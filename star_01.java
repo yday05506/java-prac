4f0134ce292f016830622966f04a76816f838f38
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-06-25 01:37:21 +0900

    [for] *을 계단식으로 출력
    *
    **
    ***
    ****
    *****

---
+++ star_01.java
@@ -0,0 +1,9 @@
+public class star_01 {
+	public static void main( String args[ ] ) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++)
+				System.out.print("*" + "\t");
+		System.out.println( );
+		}
+	}
+}
