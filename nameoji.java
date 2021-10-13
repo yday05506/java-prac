4e361d053533db8a3ddb5cbfac5346ce01c15a6d
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-10-13 23:16:02 +0900

    [array] 42로 나누고, 나머지가 다른 것들의 갯수를 구함
    (미완성)

---
+++ nameoji.java
@@ -0,0 +1,25 @@
+package IDK;
+import java.util.Scanner;
+public class nameoji {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int cnt = 0;
+		
+		System.out.print("방 갯수 입력 : ");
+		int n = sc.nextInt( );
+		int[] a = new int[n];
+		
+		System.out.print("데이터 입력 : ");
+		for(int i = 0; i < a.length; i++) 
+			a[i] = sc.nextInt( );
+		
+		for(int i = 0; i < a.length; i++) {
+			if(i % 42 != )
+				cnt++;
+		}
+		
+		System.out.print(cnt);
+	}
+
+}
